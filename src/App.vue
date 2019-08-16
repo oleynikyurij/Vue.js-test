2897a775557d65565783395d7759c09209aadf03
unknown <D:\\Mail>
2019-08-16 15:40:09 +0300

    анимация переходов

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,46 @@
 <template>
   <div id="app">
-		<div class="content">
-			<HeaderBlock></HeaderBlock>
-	    <router-view/>
-		</div>
-		<FooterBlock></FooterBlock>
+    <div class="content">
+      <HeaderBlock></HeaderBlock>
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+    </div>
+    <FooterBlock></FooterBlock>
   </div>
 </template>
 
 <script>
-import HeaderBlock from './components/HeaderBlock';
-import FooterBlock from './components/FooterBlock.vue';
+import HeaderBlock from "./components/HeaderBlock";
+import FooterBlock from "./components/FooterBlock.vue";
 export default {
-	name: 'App',
-	components: { HeaderBlock, FooterBlock },
-	
+  name: "App",
+  components: { HeaderBlock, FooterBlock }
 };
 </script>
 
 <style>
-@import './assets/css/reset.css';
-@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
-@import url('https://fonts.googleapis.com/css?family=Righteous&display=swap');
+@import "./assets/css/reset.css";
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Righteous&display=swap");
 #app {
-	display: flex;
-	flex-direction: column;
-	height: 100%;
-  font-family: 'Roboto', sans-serif;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #000;
-	}
-	.content {
-		flex-grow: 1;
-	}
+  -moz-osx-font-smoothing: grayscale;
+  color: #000;
+}
+.content {
+  flex-grow: 1;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
